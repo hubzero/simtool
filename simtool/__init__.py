38dbfd662afaece3b651a3e95c59a9bca17d9348
Steven Clark <clarks@purdue.edu>
2020-03-08 18:19:27 -0400

    setup for PyPI release 0.2.1

--- simtool/__init__.py
+++ simtool/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .utils import getGetSimToolNameRevisionFromEnvironment, findInstalledSimToolNotebooks, searchForSimTool
 from .utils import parse, findSimToolNotebook, getSimToolInputs, getSimToolOutputs
