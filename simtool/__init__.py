e5d990872b4843cd403bdbae2506f8996e29453c
Steven Clark <clarks@purdue.edu>
2021-09-22 20:08:08 -0400

    changes as result of 0.3.2 build

--- simtool/__init__.py
+++ simtool/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from .utils import getGetSimToolNameRevisionFromEnvironment, findInstalledSimToolNotebooks, searchForSimTool
 from .utils import findInstalledSimToolNotebooks as findSimTools
